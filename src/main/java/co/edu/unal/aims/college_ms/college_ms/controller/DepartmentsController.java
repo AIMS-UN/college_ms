370105ba1f1693e3c050363cab59308307de636e
SGman98 <bastiangarnica@gmail.com>
2022-10-02 16:07:53 -0500

    fix: 🐋 add changes in `docker-compose.yml`
    add network for comunication between microservice and database
    add volume for data persistence
    add environment variables for database connection

--- src/main/java/co/edu/unal/aims/college_ms/college_ms/controller/DepartmentsController.java
+++ src/main/java/co/edu/unal/aims/college_ms/college_ms/controller/DepartmentsController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value="/college/department")
+@RequestMapping(value="/college/departments")
 public class DepartmentsController {
     private final CollegeService collegeService;
     @Autowired
